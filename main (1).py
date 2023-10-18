b884d915727a192b7844b3c67dfbca2ce5b71bcc
astvu245ka22cs48 <144366350+astvu245ka22cs48@users.noreply.github.com>
2023-10-18 21:35:48 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,19 @@
-P = int( input("Please enter value for P: "))  
-Q = int( input("Please enter value for Q: "))  
-   
-# To swap the value of two variables  
-# we will user third variable which is a temporary variable  
-temp_1 = P  
-P = Q  
-Q = temp_1  
-   
-print ("The Value of P after swapping: ", P)  
-print ("The Value of Q after swapping: ", Q)
+import re
+ 
+# Sample input
+arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+x = 110
+ 
+# Convert the array to a string
+arr_str = ','.join(str(i) for i in arr)
+ 
+# Use regular expression to search for the element in the string
+match = re.search(r'\b{}\b'.format(x), arr_str)
+ 
+# Output
+if match:
+    # Calculate the index by counting the number of commas before the match
+    result = arr_str[:match.start()].count(',')
+    print(f"Element {x} is present at index {result}")
+else:
+    print(f"Element {x} is not present in the array")
